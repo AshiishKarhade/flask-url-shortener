144ea003c49c713dfcad937ad306f6d564d37069
Ashish <akarhade5@gmail.com>
2021-06-09 18:35:13 +0530

    lesson1

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello"
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return "ABOUT"
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == "POST":
+        return render_template("your_url.html", code=request.form['code'])
+    else:
+        return 'This is not valid'
 
-if __name__ == "__main__":
-    app.run()
