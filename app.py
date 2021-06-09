84c8c3fb7b38b7c177f267fb20b258be231bd296
Ashish <akarhade5@gmail.com>
2021-06-09 20:08:39 +0530

    lesson2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -50,3 +50,9 @@ def redirect_to_url(code):
                     return redirect(urls[code]['url'])
                 else:
                     return redirect(url_for('static', filename='user_files/'+urls[code]['file'] ))
+    return abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html')
