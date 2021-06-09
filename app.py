d8847788a30d3a1afb561b3b743a84732a25f8cf
Ashish <akarhade5@gmail.com>
2021-06-09 21:02:08 +0530

    final changes done, ui done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -8,7 +8,8 @@ app.secret_key = 'kjhdfkjh3234'
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes = session.keys())
+
 
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
@@ -34,6 +35,7 @@ def your_url():
 
         with open('urls.json', 'w') as json_file:
             json.dump(urls, json_file)
+            session[request.form['code']] = True
 
         return render_template("your_url.html", code=request.form['code'])
     else:
@@ -56,3 +58,8 @@ def redirect_to_url(code):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html')
+
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
